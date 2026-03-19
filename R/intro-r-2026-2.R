a30107b0ab5253641a772b8af05a320efe2614fc
musaweraryan <musawer.aryan1@gmail.com>
2026-03-19 11:39:07 -0700

    finish data cleaning

--- R/intro-r-2026-2.R
+++ R/intro-r-2026-2.R
@@ -18,3 +18,55 @@ summary(raw_15min)
 # another way
 dfSummary(raw_15min)  #best summary - shows duplicates and missing values 
 
+raw_15min[2,]
+raw_15min[ ,2]
+raw_15min[2,3]
+
+mean(raw_15min$speed) #NA showed up because there is NA in the Speed column
+hist(raw_15min$volume)
+
+
+# learn about filters
+occ_20plus <- raw_15min |> 
+  filter(occupancy > 20)  
+
+occ10_sp80 <- raw_15min |> 
+  filter(occupancy < 10 & speed > 80)
+
+
+# working with NA's
+
+blank_example <- raw_15min |> 
+  filter(is.na(speed))
+
+complete_df <- raw_15min |> 
+  filter(!is.na(speed))       # ! means is not
+
+subset_o10s80 <- occ10_sp80 |> 
+  filter(detector_id %in% c(101185, 101176))
+
+saveRDS(complete_df, "data/clean_data.rds")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
